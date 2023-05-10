3f32fc7e424ab1b7b68f6889cbcc024493ed0b38
Secludor <129930239+Secludor@users.noreply.github.com>
2023-05-10 11:37:44 +0900

    오주영/5월 1주차/3문제
    [BOJ] 2839 설탕 배달
    
    난이도 : 실버4
    알고리즘 유형 : 그리디
    내용 :
    
    5+1=6, 5+4=9, 10+2=4 임을 이용하여 조건문 활용
    3이 최소일 때 최소 운반 횟수임을 이용한 while문 구성
    [BOJ] 1715 카드 정렬하기
    
    난이도 : 골드4
    알고리즘 유형 : 그리디
    내용 :
    가장 작은 두 덱을 합치고 합친 덱을 우선순위 큐에 넣는다. 이를 덱이 하나가 될 때 까지 반복한다. (내부적으로 heap 모듈을 사용하는 PriorityQueue 클래스의 put(), get() 함수는 O(log n)의 시간 복잡도를 가진다.)
    
    [BOJ] 1202 보석 도둑
    
    난이도 : 골드2
    알고리즘 유형 : 그리디, 우선순위 큐
    내용 :
    가방에 담을 수 있는 가장 가격이 비싼 보석->가방의 개수만큼 반복
    가격 기준으로 내림차순 정렬(가격이 같으면 무게 기준 오름차순) 하고, 작은 가방부터 차례대로 대입해본다. ->시간초과 작은 가방부터 각 가방에 들어가는 보석들을 가격 기준으로 내림차순 정렬->시간초과
    최대가치 하나만 확실하면 되므로 후보 보석들을 힙 구조로 정렬-> 통과

---
+++ BOJ_2839_설탕배달.py
@@ -0,0 +1,31 @@
+﻿N=int(input())
+rest5=N%5
+q5=N//5
+if rest5:
+    if rest5==3: print(q5+1)
+    elif N>5:
+        if rest5==1: print(q5+1)
+        elif rest5==4: print(q5+2)
+        elif N>10 and rest5==2: print(q5+2)    
+        else: print(-1)
+    else: print(-1)
+else: print(q5)
+
+
+# #while문을 활용한 경우
+N = int(input())
+a = 0
+while N >= 0:
+    if N % 5 == 0:
+        a += (N//5)
+        print(a)
+        break
+    N = N - 3
+    a = a + 1
+else:
+    print(-1)
+    
+
+#인상깂었던 코드
+N = int(input())
+print([N//5+[0,1,2,1,2][N%5],-1][N in [4,7]])
