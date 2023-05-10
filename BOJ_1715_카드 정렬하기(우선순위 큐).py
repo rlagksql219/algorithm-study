cf68aa9c706fbbce2d8dc7a01bf14c52958f69e3
Secludor <129930239+Secludor@users.noreply.github.com>
2023-05-10 10:59:52 +0900

    오주영 / 5월 1주차/ 3문제
    [BOJ] 2839 설탕 배달
    
    난이도 : 실버4
    알고리즘 유형 : 그리디
    내용 :
    1.  5+1=6, 5+4=9, 10+2=4 임을 이용하여 조건문 활용
    2. 3이 최소일 때 최소 운반 횟수임을 이용한 while문 구성
    
    
    [BOJ] 1715 카드 정렬하기
    
    난이도 : 골드4
    알고리즘 유형 : 그리디
    내용 :
    가장 작은 두 덱을 합치고 합친 덱을 우선순위 큐에 넣는다. 이를 덱이 하나가 될 때 까지 반복한다.
    (내부적으로 heap 모듈을 사용하는 PriorityQueue 클래스의 put(), get() 함수는 O(log n)의 시간 복잡도를 가진다.)
    
    
    [BOJ] 1202 보석 도둑
    
    난이도 : 골드2
    알고리즘 유형 : 그리디, 우선순위 큐
    내용 :
    가방에 담을 수 있는 가장 가격이 비싼 보석->가방의 개수만큼 반복
    가격 기준으로 내림차순 정렬(가격이 같으면 무게 기준 오름차순) 하고, 작은 가방부터 차례대로 대입해본다. ->시간초과
    작은 가방부터 각 가방에 들어가는 보석들을 가격 기준으로 내림차순 정렬->시간초과
    최대가치 하나만 확실하면 되므로 후보 보석들을 힙 구조로 정렬-> 통과

---
+++ BOJ_1715_카드 정렬하기(우선순위 큐).py
@@ -0,0 +1,30 @@
+﻿import sys
+from queue import PriorityQueue
+input=sys.stdin.readline
+
+N=int(input())
+que = PriorityQueue()
+for i in range(N):
+    que.put(int(input()))
+ans=0
+for i in range(N-1):
+    dec=que.get()+que.get()
+    ans+=dec
+    que.put(dec)
+print(ans) 
+
+'''
+ 내부적으로 heap 모듈을 사용하는 PriorityQueue 클래스의
+ put(), get() 함수는 O(log n)의 시간 복잡도를 가진다.
+
+ 공부해볼 코드:
+ from heapq import*
+n,*c=map(int,open(0))
+heapify(c)
+print(sum(heappush(c,x:=heappop(c)+heappop(c))or x for _ in c[1:])
+ 
+from heapq import*
+p=heappop
+n,*h=map(int,open(0));h.sort()
+print(sum(heappush(h,t:=p(h)+p(h)) or t for _ in h[1:]))
+ '''
---
+++ BOJ_2202_보석도둑v0.3.py
@@ -0,0 +1,27 @@
+﻿import sys
+import heapq
+input=sys.stdin.readline
+
+N,K=map(int,input().split())
+catalog=list()
+for i in range(N):
+    heapq.heappush(catalog, tuple(map(int,input().split())))
+pocket=sorted(list(int(input()) for i in range(K)))
+
+ans=0
+
+hand=[]
+for c in pocket:
+    while catalog and c>= catalog[0][0]:
+        heapq.heappush(hand, -heapq.heappop(catalog)[1])
+    if hand:
+        ans-= heapq.heappop(hand)
+    elif not catalog:
+        break
+
+
+print(ans)
+'''
+변경사항: 무게당 가격기준이 아닌 가격 기준으로 변경(나누기, 곱하기, int() 횟수 감소)
+시간초과
+'''
